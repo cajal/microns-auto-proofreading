2076dea55a5149a472f5c71fbd34f3d5fc74299e
spapa013 <stelios@spapa.us>
2021-11-10 18:16:49 -0600

    adds schema_obj and clean externals paths

--- python/microns-morphology/auto_proofreading/minnie65_auto_proofreading.py
+++ python/microns-morphology/auto_proofreading/minnie65_auto_proofreading.py
@@ -1,11 +1,12 @@
 import datajoint as dj
+from datajoint import datajoint_plus as djp
 
 from microns_morphology_api import config
-schema_name = 'microns_minnie65_auto_proofreading'
+schema_obj = config.SCHEMAS.MINNIE65_AUTO_PROOFREADING
 
-config.register_adapters(schema_name, context=locals())
-config.register_externals(schema_name)
+config.register_adapters(schema_obj, context=locals())
+config.register_externals(schema_obj)
 
-schema = dj.schema(schema_name)
+schema = dj.schema(schema_obj.value)
 schema.spawn_missing_classes()
 
--- python/microns-morphology/minnie_morphology/minnie65_morphology.py
+++ python/microns-morphology/minnie_morphology/minnie65_morphology.py
@@ -1,10 +1,11 @@
 import datajoint as dj
+from datajoint import datajoint_plus as djp
 
 from microns_morphology_api import config
-schema_name = 'microns_minnie65_morphology'
+schema_obj = config.SCHEMAS.MINNIE65_MORPHOLOGY
 
-config.register_adapters(schema_name, context=locals())
-config.register_externals(schema_name)
+config.register_adapters(schema_obj, context=locals())
+config.register_externals(schema_obj)
 
-schema = dj.schema(schema_name)
+schema = dj.schema(schema_obj.value)
 schema.spawn_missing_classes()
