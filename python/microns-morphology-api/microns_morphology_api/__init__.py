52995abee03f9f6b00aa2623e6ed3cf7797ba318
spapa013 <stelios@spapa.us>
2021-11-15 16:31:04 -0600

    updates package __init__ for version tracking

--- python/microns-morphology-api/microns_morphology_api/__init__.py
+++ python/microns-morphology-api/microns_morphology_api/__init__.py
@@ -1,10 +1,3 @@
-import warnings
-from microns_utils import config_utils
+from microns_utils import version_utils
 
-_repo = 'microns-morphology'
-_package = 'microns-morphology-api'
-
-try:
-    __version__ = config_utils.get_package_version(repo=_repo, package=_package)
-except:
-    warnings.warn('Package version not able to be determined.')
+__version__ = version_utils.get_package_version(repo='microns-morphology', package='microns-morphology-api')
