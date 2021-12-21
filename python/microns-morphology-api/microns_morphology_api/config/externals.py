e6d18d380ca8f42de1665932a70d170137f4deb6
spapa013 <stelios@spapa.us>
2021-12-21 11:57:58 -0600

    make_store_dict from djp

--- python/microns-morphology-api/microns_morphology_api/config/externals.py
+++ python/microns-morphology-api/microns_morphology_api/config/externals.py
@@ -3,7 +3,7 @@ Externals for DataJoint tables.
 """
 
 from pathlib import Path
-from microns_utils import config_utils
+import datajoint.datajoint_plus as djp
 
 '''
 minnie_em_external_stack_path = Path() / '/mnt' / 'dj-stor01' / 'microns'/ 'minnie' / 'stacks'
@@ -21,12 +21,11 @@ h01_morphology_external_skeletons_path = base_path / 'h01' / 'skeletons'
 h01_morphology_external_meshes_path = base_path / 'h01' / 'meshes'
 h01_morphology_external_faces_path = base_path / 'h01' / 'faces'
 h01_morphology = {
-    'h01_somas': config_utils.make_store_dict(h01_morphology_external_somas_path),
-    'h01_skeletons': config_utils.make_store_dict(h01_morphology_external_skeletons_path),
-    'h01_meshes': config_utils.make_store_dict(h01_morphology_external_meshes_path),
-    'h01_faces': config_utils.make_store_dict(h01_morphology_external_faces_path),
-    
-    }
+    'h01_somas': djp.make_store_dict(h01_morphology_external_somas_path),
+    'h01_skeletons': djp.make_store_dict(h01_morphology_external_skeletons_path),
+    'h01_meshes': djp.make_store_dict(h01_morphology_external_meshes_path),
+    'h01_faces': djp.make_store_dict(h01_morphology_external_faces_path),
+}
 
 #h01_auto_proofreading
 h01_auto_proofreading_external_decomposition_path = base_path / 'h01' / 'decomposition'
@@ -34,12 +33,11 @@ h01_auto_proofreading_external_faces_path = base_path / 'h01' / 'faces'
 h01_auto_proofreading_external_skeletons_path = base_path / 'h01' / 'skeletons'
 h01_auto_proofreading_external_meshes_path = base_path / 'h01' / 'meshes'
 h01_auto_proofreading = {
-    'h01_decomposition': config_utils.make_store_dict(h01_auto_proofreading_external_decomposition_path),
-    'h01_faces': config_utils.make_store_dict(h01_auto_proofreading_external_faces_path),
-    'h01_skeletons': config_utils.make_store_dict(h01_auto_proofreading_external_skeletons_path),
-    'h01_meshes': config_utils.make_store_dict(h01_auto_proofreading_external_meshes_path),
-    
-    }
+    'h01_decomposition': djp.make_store_dict(h01_auto_proofreading_external_decomposition_path),
+    'h01_faces': djp.make_store_dict(h01_auto_proofreading_external_faces_path),
+    'h01_skeletons': djp.make_store_dict(h01_auto_proofreading_external_skeletons_path),
+    'h01_meshes': djp.make_store_dict(h01_auto_proofreading_external_meshes_path),
+}
 
 #minnie65_morphology
 minnie65_morphology_external_somas_path = base_path / 'minnie' / 'somas'
@@ -47,12 +45,11 @@ minnie65_morphology_external_skeletons_path = base_path / 'minnie' / 'skeletons'
 minnie65_morphology_external_meshes_path = base_path / 'minnie' / 'meshes'
 minnie65_morphology_external_faces_path = base_path / 'minnie' / 'faces'
 minnie65_morphology = {
-    'minnie65_somas': config_utils.make_store_dict(minnie65_morphology_external_somas_path),
-    'minnie65_skeletons': config_utils.make_store_dict(minnie65_morphology_external_skeletons_path),
-    'minnie65_meshes': config_utils.make_store_dict(minnie65_morphology_external_meshes_path),
-    'minnie65_faces': config_utils.make_store_dict(minnie65_morphology_external_faces_path),
-    
-    }
+    'minnie65_somas': djp.make_store_dict(minnie65_morphology_external_somas_path),
+    'minnie65_skeletons': djp.make_store_dict(minnie65_morphology_external_skeletons_path),
+    'minnie65_meshes': djp.make_store_dict(minnie65_morphology_external_meshes_path),
+    'minnie65_faces': djp.make_store_dict(minnie65_morphology_external_faces_path),
+}
 
 #minnie65_auto_proofreading
 minnie65_auto_proofreading_external_decomposition_path = base_path / 'minnie' / 'decomposition'
@@ -60,9 +57,8 @@ minnie65_auto_proofreading_external_faces_path = base_path / 'minnie' / 'faces'
 minnie65_auto_proofreading_external_skeletons_path = base_path / 'minnie' / 'skeletons'
 minnie65_auto_proofreading_external_meshes_path = base_path / 'minnie' / 'meshes'
 minnie65_auto_proofreading = {
-    'minnie65_decomposition': config_utils.make_store_dict(minnie65_auto_proofreading_external_decomposition_path),
-    'minnie65_faces': config_utils.make_store_dict(minnie65_auto_proofreading_external_faces_path),
-    'minnie65_skeletons': config_utils.make_store_dict(minnie65_auto_proofreading_external_skeletons_path),
-    'minnie65_meshes': config_utils.make_store_dict(minnie65_auto_proofreading_external_meshes_path),
-    
-    }
+    'minnie65_decomposition': djp.make_store_dict(minnie65_auto_proofreading_external_decomposition_path),
+    'minnie65_faces': djp.make_store_dict(minnie65_auto_proofreading_external_faces_path),
+    'minnie65_skeletons': djp.make_store_dict(minnie65_auto_proofreading_external_skeletons_path),
+    'minnie65_meshes': djp.make_store_dict(minnie65_auto_proofreading_external_meshes_path),
+}
